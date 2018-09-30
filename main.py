a596a939994d465c5e800d7561c41c2fb900a5e0
travis-cramer <travis@ridecell.com>
2018-09-30 13:25:13 -0700

    adding logs for debugging

--- main.py
+++ main.py
@@ -56,9 +56,13 @@ def mimic_me(handle):
 	# get and analyze the past (up to) 1000 tweets made by the user-to-be-mimicked
 	statuses = twitter_api.GetUserTimeline(screen_name=handle, count=1000, include_rts=False)
 
+	print("here 1")
 	text = get_text(statuses)
+	print("here 2")
 	text = remove_handles(text)  # take out all mentions
+	print("here 3")
 	text = remove_bad_chars(text)  # remove "bad" characters, like parentheses
+	print("here 4")
 
 	# cannot perform markov chain generation when there are less than 100 words in total to be analyzed
 	if len(text.split(' ')) <= 100:
@@ -69,10 +73,15 @@ def mimic_me(handle):
 	mc.add_string(text)
 	result = mc.generate_text()
 
+	print("here 5")
+
 	# format the result
 	result = capitalize_first_word(result)
+	print("here 6")
 	result = add_period_to_the_end(result)
+	print("here 7")
 	new_tweet = list_of_words_to_string(result)
+	print("here 8")
 
 	# ensure that new tweet is 240 characters or less
 	while len(new_tweet) > 240:
@@ -92,6 +101,9 @@ def main():
 		new_mention = True
 
 		if ('mimic me' in mention.text.lower()):
+
+			print("Saw a mimic request")
+
 			# generate mimicking tweet
 			result = mimic_me(mention.user.screen_name)
 			
@@ -101,13 +113,14 @@ def main():
 				print 'New mimic: @%s' %(mention.user.screen_name)
 				update_since_id(status.id)
 			elif result == 0:
+				print("making sorry response")
 				try:
 					status = twitter_api.PostUpdate(status=('@%s '%(mention.user.screen_name) + ' ' + SORRY_RESPONSE),
 										   			in_reply_to_status_id=mention.id)
 					print "Made sorry response to @%s" % (mention.user.screen_name)
 					update_since_id(status.id)
 				except twitter.error.TwitterError:
-					pass
+					print("Duplicate Sorry response error")
 		else:
 			try:
 				status = twitter_api.PostUpdate(status=('@%s '%(mention.user.screen_name) + ' ' + INFORMATION_RESPONSE),
@@ -115,23 +128,24 @@ def main():
 				print "Made an informational response to @%s" %(mention.user.screen_name)
 				update_since_id(status.id)
 			except twitter.error.TwitterError:
-				pass
+				print("Duplicate informational response error")
 
 	if verbose and not new_mention:
 			print 'No new mentions.'
 
 
-
-if forever:
-	# infinitely loop while running script
-	while True:
-		if verbose:
-			print '-----------------------------------------------------'
-			print dt.datetime.now().strftime("%Y-%m-%d %H:%M")
+if __name__ == "__main__":
+	if forever:
+		# infinitely loop while running script
+		while True:
+			if verbose:
+				print '-----------------------------------------------------'
+				print dt.datetime.now().strftime("%Y-%m-%d %H:%M")
+			main()
+			# sleep for 5~ minutes before checking twitter again for new mentions
+			if verbose:
+				print 'Now waiting 5 minutes...'
+				time.sleep(300)
+	else:
+		print("Made it into __main__ block")
 		main()
-		# sleep for 5~ minutes before checking twitter again for new mentions
-		if verbose:
-			print 'Now waiting 5 minutes...'
-			time.sleep(300)
-else:
-	main()
